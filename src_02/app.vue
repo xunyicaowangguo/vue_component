d882c5e98179f30471ccad899de515f06b20cf44
Your Name <email@example.com>
2020-01-03 20:53:41 +0800

    w

---
+++ src_02/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h2>{{msg}}</h2>
+      <HelloWorld/>
+      <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld'
+  export default {
+      data(){
+          return{
+              msg:'First Vue Component!'
+          }
+      },
+
+      components:{
+          HelloWorld
+      }
+  }
+</script>
+
+<style scoped>
+    h2{
+        color: #899;
+    }
+ 
+</style>
